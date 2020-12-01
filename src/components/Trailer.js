61de033bd9840557bbb55b27df14241a0ef11f28
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-11-30 16:28:18 -0800

    updated video height and farhan's page

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -3,7 +3,7 @@ import './Trailer.css'
 export default function Trailer() {
     return (
         <div className="trailer-container">
-            <iframe width="1280" height="720" src="https://www.youtube.com/embed/_uU9GMJ8a5w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="720" height="420" src="https://www.youtube.com/embed/_uU9GMJ8a5w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
