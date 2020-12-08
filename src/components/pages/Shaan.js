3d9105ce02c929a23a4dc0f3476193257c144abd
shaannessy25 <shaan.hurley25@gmail.com>
2020-12-07 22:40:54 -0800

    final update

--- src/components/pages/Shaan.js
+++ src/components/pages/Shaan.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../App.css';
+import './Shaan.css'
 
 export default function Shaan() {
   return (
@@ -17,13 +17,22 @@ export default function Shaan() {
           dependent women. Woman groups lobbied for many different reforms and causes including the disarmament and the peace movement. This movement
           was against violence and guns. Women were able to accomplish many tasks that seemed unachievable for many.
       </p>
+      <img src="https://www.nps.gov/bepa/learn/images/Alice-Paul-raises-glass1az.jpg?maxwidth=1200&maxheight=1200&autorotate=false"  className='shaan-image' alt="not working"/>
       <p>
         The 1920s was also a time of  promiscuity, this lead to the first documented sex revolution of american history. Women no longer felt the need to be "ladylike", 
         instead women would go to bars, drink with the men, dress provocatively, and sometimes dance on tables. Women embodied new roles, fashion, lifestyle, etc.
-         that were radical to the original womanhood, this new generation became to be known as 'the new women'. Women no longer found the traditional 
-         lifestyle glamourous. It was much cooler to go against those norms. Women started to have more sex, less marriages, and less babies. Most women avoided 
-         motherhood all together by getting illegal abortions or taking contraceptives. 
+        that were radical to the original womanhood, this new generation became to be known as 'the new women'. Women no longer found the traditional motherhood
+        lifestyle glamourous. It was much cooler to go against those norms. Women started to have more sex, less marriages, and less babies. Most women avoided 
+        motherhood all together by getting illegal abortions or taking contraceptives. Womens fashion made radical reforms during this era; a new style of dress was created
+        to be looser and reveal more skin. This dress became to be known as the flapper dress and it was one of the more popular outfits in this era. Women no longer felt the stigma
+        for acting unlady like, instead they saw it as an opportunity to push this social agenda of female freedom.
+        
       </p>
+      <img src="https://img.huffingtonpost.com/asset/5b9d297a2200005600d98480.jpeg?ops=scalefit_960_noupscale" className='shaan-image2' alt='not working' />
+      <h3>Works Cited</h3>
+      <ul>
+        <li>Dumenil, Lynn. “The New Woman and the Politics of the 1920s.” OAH Magazine of History, vol. 21, no. 3, July 2007, pp. 22–26. EBSCOhost, doi:10.1093/maghis/21.3.22.</li>
+      </ul>
     </div>
   )
 
