c3445f44ce5e2e67345c5ea8a9118e4460a6e43e
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-12-10 21:37:40 -0800

    updated my tab

--- src/components/pages/Farhan.js
+++ src/components/pages/Farhan.js
@@ -5,152 +5,89 @@ export default function Farhan() {
   return <div className="Farhan">
 
     <div className="con">
+    <h1 className="title"> Temperance Movement</h1>
+
       <img className="temp" src='images/Temperance.jpg'></img>
-      <h1 className="title"> Temperance Movement</h1>
       <p>
-      The Temperance movement started before the Eighteenth Amendment. At the time many people started to dispute alcohol. People believed that alcohol was ruining and damaging families financially.
-      Women claimed men would drink their family's money away. This temperance movement influenced women to join the Prohibition movement. Women felt as they had to join the movement because they were dealing with drunk husbands and fathers in their household.
-      In such a way it allowed women the be part of politics during the time period.
-      One major group that pushed for Prohibition was the Woman's Christian Temperance Union and On the other end of the spectrum was the Women's Organization for National Prohibition Reform.
-      The Womens Organization for National Prohibition Reform was heavily involved in getting the 18th Amendment repealed. The group disputed that the Prohibition took rights away from citizens,
-     and that the prohibition is ineffective because bootleggers.
-
-      </p>
-    </div>
-
-    <div className="con">
-      <img className="temp" src='images/WomensCrusade.jpg'></img>
+      <ul className="list">
+        <li>The Temperance movement started before the Eighteenth Amendment. </li>
+        <li>During the era, many people disputed alcohol.</li>
+        <li>People believed that alcohol was ruining and damaging families financially. </li>
+        <li>This lead to women joining the Prohibition movement. </li>
+        <li> One major group that pushed for Prohibition was the Woman's Christian Temperance Union and On the other end was the Women's Organization for National Prohibition Reform.
+        </li>
+        <li> The Women’s Organization for National Prohibition Reform was heavily involved in getting the 18th Amendment repealed.</li>
+        <li>
+          The group disputed the Prohibition took rights away from citizens, and that the prohibition is ineffective because of bootleggers.
+        </li>
+        </ul>
+      
       <h1 className="title"> Women's Crusade</h1>
-      <p>
-   Before the Women's Christian Temperance Union, there was the  Women's Crusade.
-     Their aim was to end alcoholism as they saw it as detrimental disease to their society. In 1874, the Crusade was founded in Ohio and it gave chance for women to promote national temperance.
-     The movement began when Dr. Diocletian Lewis gave lectures, telling the story of how his mother went to the saloons and prayed and sang songs. Which caused the saloons to close.
-      At first, however this approach didn't have much success.
-    The movement had some success in Hillsboro, Ohio, There men and women learned from Dr. Diocletian and began following his ways. Men provided money and emotional support, although the focul point of the movement was women.
-  after another one of Lewis' lectures the movement really began to take off.
+      <img className="temp" src='images/WomensCrusade.jpg'></img>
+      <ul className="list">
+        <li>Before the Women's Christian Temperance Union, there was the Women's Crusade. 
+        </li>
+        <li>Their aim was to end alcoholism as they saw it as a detrimental disease to their society. 
+        </li>
+        <li>In 1874, the Crusade was founded in Ohio </li>
+        <li> The movement began when Dr. Diocletian Lewis gave lectures </li>
+    
+        <li>He claimed his mother went to the saloons and prayed and sung songs. Which lead to the closing of saloons</li>
+        <li>
+        Crusades were really successful in small cities. 
+        </li>
+        <li>There were approximately 700 breweries closed across the country. </li>
+        <li>
+        Their tactics included picketing, openly praying and singing, marching in the streets, and asking local shopkeepers and saloon owners for pledges. 
+        </li>
+        </ul>
+      
 
 
+<h1 className="title">Bootleggers</h1>
+<img className="temp" src='images/bootleg.jpg'></img>
+<ul className="list">
+        <li>In U.S. history, bootlegging was the illegal manufacture, transport, distribution, or sale of alcoholic beverages during the Prohibition period
+        </li>
+        <li>Those activities were forbidden under the Eighteenth Amendment in (1919) to the U.S. Constitution. 
+        </li>
+        <li>Most recorded bootleggers were men </li>
+        <li> There were female bootleggers, but approximately 170  cases were recorded. </li>
+    
+        <li>Most female bootleggers were mothers who were trying to financially support their families </li>
+        <li>
+        When arrested women appealed they needed to provide for their family. 
+ High risk of mothers losing custody of their children 
+
+        </li>
+       
+        </ul>
+      
 
 
-  Crusades were successful in small towns all over Ohio, Michigan, Indiana, Pennsylvania, and New Jersey erupted after Hillsboro.
-There were 750 breweries closed across the country, and a lot of shopkeepers sold alcohol only to those with prescriptions.
-The movement's tactic was to picket, openly pray and sing, march in the streets, and ask local shopkeepers and saloon owners for pledges.
-Saloons that were especially harsh against the crusaders would always have women standing in front of their doors, blocking entry, as was the case after the saloon owner locked two of the women leaders in the saloon at a saloon in Adrian, Michigan.
-The campaign was not endorsed by all women. Some women spat alongside their male companions at the crusaders, either because they felt it was not the place of a woman to behave so openly, or because they did not help them.
-From temperance.Both women and men, whatever the cause, saw alcohol as a serious moral problem and helped the crusaders.  
-     </p>
-    </div>
-    <div className="con">
-      <img className="temp" src='images/populistparty.jpg'></img>
-      <h1 className="title">Populist Party</h1>
-      <p>
-      The populist party faced many conflicts within the group. Although most individuals were for the prohibition reform. Even though they never announced where they stood on their view of the movement as a collective group. Francis E Williard motivated the populist party to make an official statement. She also tried the combine WCTU with the Prohibition party because they made an impact on Populist members. 
-      She was not successful with her attempt because the Prohibity didn’t think it was necessary to support Women’s Suffrage on top of what they have accomplished try to reform.   </p>
-    </div>
-    {/* <div className="con">
-      <img className="temp" src='images/TWCTU.jpg'></img>
-
-      <h1 className="title">The Women's Christian Temperance Union</h1>
-      <p>
-      On November 18, 1874, in Cleveland, Ohio, the Women's Christian Temperance Union was organized. It soon became the largest association for women in the United States. The women in the movement were inspired in the United States by the extreme drinking epidemic and the irrational ills that befell women whose husbands were drunkards. 
-It was seen as both a moral and domestic challenge, enabling women to unprecedentedly enter the political sphere .
-Under the revolution, men were barred from voting and taking office, though they were asked to contribute financially. 
-In 1874, Annie Wittenmyer founded the Women's Christian Temperance Union. In 1879, the new president was Frances E. Willard, who remained president until her death. 
-The corporation did not concentrate only on temperance, but also encouraged other social restrictions and the issue of equality for women. "These other problems were part of Willard's policy of "Do Something.
-Under a popular chief, Willard, the WCTU was organized, but had considerable autonomy for local chapters. In order to support the WCTU, Willard also traveled the country and visited local chapters. She visited Texas in 1882 and California in 1883. These visits stimulated rapid growth in membership growth. </p>
-    </div>
-
-
-
-    <div className="con">
-      <img className="temp" src='images/Annie.jpg'></img>
-      <h1 className="title">Annie Wittenmyer</h1>
-      <p>
-        Annie Wittenmyer was born Annie Turner in Ohio in 1827. After the Civil War, Annie aligned herself with the Women's Crusade.
-        She went to a convention in Cleveland, Ohio, where the Women's Christian Temperance Union was formed and she was voted as the first official president.
-        Annie worked closely with Frances Willard, who was the secretary. They had opposing views regarding the WCTU's involvement in women's suffrage, so Annie became president of a new faction,
-        the Non-Partisan Women's Christian Temperance Union
       </p>
     </div>
     <div className="con">
-      <img className="temp" src='images/Frances.jpg'></img>
-      <h1 className="title">Frances Willard</h1>
-      <p>
-        Frances Willard was born September 28, 1839 in New York. She was a founder of the Women's Temperance Union and President from 1879 until her death in 1898.
-        Willard was a very spiritual woman due to her upbringing and a brush with death when she was 19. She was very interested in the Women's Crusade, and eventually
-        became the President of the Chicago chapter of the Women's Christian Temperance Union, despite a higher paying job offer at a college.
-        Five years later she was voted into the Presidency of the entire Women's Christian Temperance Union. As the President, she gained national recognition and was praised for her competence.
-        Frances Willard toured Texas in 1882 in order to organize WCTU chapters. It was at the end of a larger tour through the South. The movement struggled in the more conservative southern states,
-        but her tour garnered large crowds and some success. It did not achieve long-term success and Texas prohibitionists maintained their separation from the northern based prohibition movement and Willard herself.
-      </p>
-    </div>
-    <div className="con">
-      <img className="temp" src='images/Carrie.jpg'></img>
-      <h1 className="title">Carrie A. Nation</h1>
-      <p>
-        Carrie Nation, originally Carrie Amelia Moore, was born in Kentucky in 1846. Her name is also sometimes spelled Carry.She is most famous for her extreme opposition of alcohol and taking action by destroying bars and saloons with a hatchet.
-        Carrie was very religious and would often pray or sing while swinging her hatchet across the United States. During her time and in history books, Carrie has been depicted as unnecessarily violent and domineering in her support of the Prohibition movement.
-        No group claims that she was a part of what they stood for (feminists, various Christian religions, the WCTU).
-        Carrie also published The Smasher’s Mail newspaper where people sent in their reactions to the destroying of saloons,
-        which Carrie saw as a noble cause. Some people praised her actions, while others were more than angry with the way she handled violations of the Prohibition.
-      </p>
-    </div>
 
-    <div className="con">
-      <img className="temp" src='images/Pauline.jpg'></img>
-      <h1 className="title">Pauline Morton Sabin</h1>
-      <p>
-        Pauline Morton Sabin was born in 1887 to a wealthy family. After her divorce and subsequent remarriage to Charles Hamilton Sabin, she became involved in charity and political work.
-        She organized Republican meetings and parties on her estate, leading her to eventually found the National Women’s Republican Club in 1921. She was the first woman to serve on the National Republican Committee.
-        She continued work in politics, eventually campaigning for Herbert Hoover in the 1928 election. Her work in Prohibition was inspired by her distaste for the hypocrisy of politicians who supported Prohibition only
-        in public and the clear ineffectiveness of the law. She had supported Herbert Hoover, a Prohibitionist, but after his inaugural address decided to organize the anti-Prohibitionist women of America against the party’s platform.
-      </p>
-    </div>
+    <h1 className="title"> Works Cited</h1>
+      <ul className="list">
+      <li>
+        Chused, Richard H. “The Temperance Movement’s Impact on Adoption of Women’s Suffrage.” Akron Law Review, vol. 53, no. 2, Dec. 2019, pp. 359–386. EBSCOhost, search.ebscohost.com/login.aspx?direct=true&db=a9h&AN=143792733&site=ehost-live.
+       </li>      
+      </ul>
+      </div>
+        
+       </div>
+        
+    
 
 
-    <div className="con">
+    
 
-      <img className="temp" src='images/bootleg.jpg'></img>
-      <h1 className="title">Bootleggers</h1>
-      <p>
-        In U.S. history, bootlegging was the illegal manufacture, transport, distribution, or sale of alcoholic beverages during the Prohibition period (1920–33), when those activities were forbidden under the Eighteenth Amendment (1919) to the U.S. Constitution.
-        Most recorded bootleggers were men involved in shady business that had connections to crime lords, but women were typically overlooked in
-        this particular practice. There were many female bootleggers, but only 173 individual cases were recorded. Of those recorded cases,
-        there were certain demographic patterns. Most female bootleggers were mothers who were trying to financially support their families,
-        whether they were widows or not. Mothers usually stayed home to take care of their children and brewing alcohol was not a challenge with privacy
-        and access to kitchens. A majority of female bootleggers were immigrants. They felt more justified in their actions since they had come from various
-        cultures that did not see
-        the creation or consumption of alcohol as an issue.[17]
-        When arrested for illegal sales of alcohol most women admitted to their actions, but appealed to the judge that their motives were to provide their children with basic necessities.
-        However, there was a high risk of mothers losing custody of their children when they were arrested, even if there was nobody left to care for them while their mother served a jail sentence.
-         </p>
-    </div >
+    
+   
 
-    <div className="con">
 
-      <img className="temp" src='images/saloon.jpg'></img>
-      <h1 className="title">Changes in Social Life</h1>
-      <p>
-        Before Prohibition women generally stayed away from saloons and bars, mostly drinking behind the closed doors of their own homes. During Prohibition, however, women started occupying more public areas such as speakeasies. Breaking rules seemed to appeal to a large population of women and drinking in a public setting was no longer limited to those considered to have low morals.
-      </p>
-    </div>
-    <div className="con">
-      <img className="temp" src='images/smasher.jpg'></img>
-      <h1 className="title"> Reactions to the Movement</h1>
-      <p>
-        Reactions to the Movement
-        There were mixed reactions to the movement. Some thought that they were pursuing a noble cause, but others believed that Prohibition was a failure and an overreach on the part of the government.
-        The newspaper Carrie Nation was an editor for in Topeka, Kansas was titled The Smasher’s Mail and published scathing reviews of Nation's actions the first week of February, 1901. Some of those that wrote in claimed that God was in favor of saloons and criticized her for saying she was a follower of God. Others praised her for her actions.</p>
-    </div>
-    <div className="con">
-      <img className="temp" src='images/politics.png'></img>
-      <h1 className="title">Prolitical Views, and Class Divides</h1>
-      <p>
-        Most African Americans opposed Prohibition. However, African American women felt that they needed to remain loyal to the Republican party that had put an end to slavery and gave them more rights as citizens. They were torn between their desire for alcohol and their loyalty to a party that supported the Prohibition. They also believed that the Democratic party was using their opposition to Prohibition to attract more African American voters which would eventually lead to the organization of oppression.[19]
-        Class Divides
-        There is controversy over class divides in the support for Prohibition. Some claim that working class-women supported Prohibition because they viewed alcohol as the vice keeping them in poverty. Their husbands would go and drink their money away, leaving them without the money to buy food and other supplies. Others claim that working-class women, along with their husbands, were for repeal because of the new power it gave to police forces to interfere with their lives.[20]
-      </p>
-    </div> */}
-  </div >
+
 
 }
