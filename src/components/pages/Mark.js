8de0e238639104a088ccd966536a6c3a07f40c21
Donghyun (Mark) Kim <markk628@gmail.com>
2020-12-10 23:02:29 -0800

    some fucked up styling going on

--- src/components/pages/Mark.js
+++ src/components/pages/Mark.js
@@ -5,6 +5,12 @@ export default function Mark() {
   return (
   <div className="markeezy">
     <img className="mark" src='images/Prohibition.jpg'></img>
+    <div className="cited">
+      <h1>Sources</h1>
+      <p>Casper, Juliet M. “Organized Crime--United States.” Journal of Criminal Law & Criminology, vol. 85, no. 3, Winter 1995, p. 837. EBSCOhost, search.ebscohost.com/login.aspx?direct=true&db=a9h&AN=9507161199&site=ehost-live.</p>
+      <p>Chused, Richard H. “The Temperance Movement’s Impact on Adoption of Women’s Suffrage.” Akron Law Review, vol. 53, no. 2, Dec. 2019, pp. 359–386. EBSCOhost, search.ebscohost.com/login.aspx?direct=true&db=a9h&AN=143792733&site=ehost-live.</p>
+      <p>Woodwiss, Michael. “CAPONE TO KEFAUVER: Organised Crime in America.” History Today, vol. 37, no. 6, June 1987, p. 8. EBSCOhost, search.ebscohost.com/login.aspx?direct=true&db=a9h&AN=4870268&site=ehost-live.</p>
+    </div>
   </div>
   )
 }
